e902cc5bd7e5d4af672806e46dff08f98e0834af
Amitabh <121345266+Kadetron@users.noreply.github.com>
2023-02-12 23:55:26 +0530

    Update madox.sh

--- madox.sh
+++ madox.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 declare -u choice
 declare -i n
+echo -n "Enter the name: "
+read name
+name=$(echo $name | sed "s/ /%20/g")
 echo -n "anime or manga[a/m]: "
 read choice
-echo $choice
 declare c
 if [ "$choice" == "A" ]
 then
@@ -15,13 +17,10 @@ else
         echo wrong input
         exit 5
 fi
-echo $c
-#cat 1st.txt | jq '.categories[] | select(.type == "manga") | .items[] | {name:.name, media_type:.payload.media_type}'
-#curl 'https://myanimelist.net/search/prefix.json?type=all&keyword=solo%leveling' | jq --arg a "$c" '.categories[] | select(.type==$a) | .items[] | {name:.name,media_type:.payload.media_type}'
-cat 1st.txt | jq --arg a "$c" '.categories[] | select(.type==$a) | .items[] | {name:.name,media_type:.payload.media_type}'
-idarray=( $(cat 1st.txt | jq --arg a "$c" '.categories[] | select(.type == $a) | .items[] | .id') )
-echo ${#idarray[@]}
+res=$(curl -s "https://myanimelist.net/search/prefix.json?type=$c&keyword=$name")
+echo $res | jq '.categories[] | .items[] | .name, .payload.media_type' | sed -E -e "s/^[{|}]//g" -e "s/[\"]//g" -e "s/[\,]$//g" | awk '{getline b;printf("%s (%s)\n",$0,b)}' | awk '{print "[" NR-0 "] " $0}'
+idarray=( $(echo $res | jq --arg a "$c" '.categories[] | select(.type == $a) | .items[] | .id') )
 echo -n "Enter choice:"
 read n
 echo ${idarray[@]}
-cat 1st.txt | jq --argjson a "${idarray[ (($n-1)) ]}" --arg s "$c" '.categories[] | select(.type == $s) | .items[] | select(.id == $a)'
+echo $res | jq --argjson a "${idarray[ (($n-1)) ]}" '.categories[] | .items[] | select(.id == $a)'
